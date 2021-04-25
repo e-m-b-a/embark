5d3966506758e7216b7e055c15106c9ca371ac8a
Maximilian Wagner <maximilian.b.wagner@fau.de>
2021-04-25 19:02:18 +0200

    Add linter pipeline to check conformity with pep8. Add test pipeline to run unittests. Add senseless unit test for testing pipeline.

--- embark/embark/settings.py
+++ embark/embark/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'embark.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
