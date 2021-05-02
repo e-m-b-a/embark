975e3dbccc07520ac76bdce3525212ef00646f37
Garima Chauhan <ashutosh.singh.de@gmail.com>
2021-05-02 18:12:24 +0200

    Fix lint errors

--- embark/embark/settings.py
+++ embark/embark/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'embark.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('DATABASE_NAME'),
-            'USER': os.environ.get('DATABASE_USER'),
-            'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-            'HOST': os.environ.get('DATABASE_HOST'),
-            'PORT': os.environ.get('DATABASE_PORT'),
-            'CONN_MAX_AGE': 300,
-            'OPTIONS': {'charset': 'utf8mb4'},
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+        'CONN_MAX_AGE': 300,
+        'OPTIONS': {'charset': 'utf8mb4'},
+    },
 }
 
 
