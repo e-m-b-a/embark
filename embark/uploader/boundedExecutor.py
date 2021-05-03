abfe9007da7e9c0ef17c8d2e3772e8fa93514cf8
Maximilian Wagner <maximilian.b.wagner@fau.de>
2021-05-04 01:16:39 +0200

    Added boundedExecutor with limited queue size and non blocking waiting. Not tested for emba

---
+++ embark/uploader/boundedExecutor.py
@@ -0,0 +1,65 @@
+import logging
+from concurrent.futures.thread import ThreadPoolExecutor
+from subprocess import Popen
+from threading import BoundedSemaphore
+
+
+class boundedExecutor:
+    def __init__(self, bound, max_workers):
+
+        # assign the threadpool max_worker_threads
+        self.executor = ThreadPoolExecutor(max_workers=max_workers)
+        # create semaphore to track queue state
+        self.semaphore = BoundedSemaphore(bound + max_workers)
+
+        # emba directories
+        self.emba_script_location = "/app/emba/emba.sh"
+        self.emba_log_location = "/app/emba/log_%s"
+
+    def run_shell_cmd(self, cmd):
+        # TODO emba.log analyzer needs to be started
+        emba_process = Popen(cmd, shell=True).wait()
+
+    def submit_firmware(self, firmware):
+
+        # TODO extract information from parameter
+        image_file_location = 0
+        image_file_name = "img"
+        log_file_location = 0
+
+        real_emba_log_location = (self.emba_log_location, image_file_name)
+        emba_flags = "-D -i -g -s -z -W"
+
+        emba_cmd = ("%s -f %s -l %s %s", real_emba_log_location, image_file_location, log_file_location, emba_flags)
+
+        emba_fut = self.executor.submit(self.run_shell_cmd, emba_cmd)
+
+        if not emba_fut:
+            # TODO handle full queue
+            pass
+
+        return emba_fut
+
+    """See concurrent.futures.Executor#submit"""
+
+    def submit(self, fn, *args, **kwargs):
+
+        queue_not_full = self.semaphore.acquire(blocking=False)
+        if not queue_not_full:
+            logging.error("Executor queue full")
+            return None
+
+        try:
+            future = self.executor.submit(fn, *args, **kwargs)
+        except Exception as e:
+            logging.error("Executor task could not be submitted")
+            self.semaphore.release()
+            raise e
+        else:
+            future.add_done_callback(lambda x: self.semaphore.release())
+            return future
+
+    """See concurrent.futures.Executor#shutdown"""
+
+    def shutdown(self, wait=True):
+        self.executor.shutdown(wait)
---
+++ embark/uploader/test_boundedExecutor.py
@@ -0,0 +1,28 @@
+import pytest
+from django.test import TestCase
+
+from .boundedExecutor import boundedExecutor
+
+
+class test_boundedExecutor(TestCase):
+
+    def setUp(self):
+        self.boundExecutor = boundedExecutor(bound=2, max_workers=2)
+
+    # TODO: maybe add timeout
+    def test_non_blocking_overflow(self):
+
+        fut_list = []
+
+        for _ in range(4):
+            # if testing under windows use "timeout /T 5" instead of "sleep 5"
+            fut = self.boundExecutor.submit(self.boundExecutor.run_shell_cmd, "sleep 5")
+            self.assertIsNotNone(fut)
+            fut_list.append(fut)
+
+        for _ in range(2):
+            fut = self.boundExecutor.submit(self.boundExecutor.run_shell_cmd, "sleep 5")
+            self.assertIsNone(fut)
+
+        for fut in fut_list:
+            fut.result()
