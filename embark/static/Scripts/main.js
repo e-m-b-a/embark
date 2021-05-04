88968f352b39a16484cbe2e0f03bcdb2e984cfbb
uk61elac <vaishnavi.umesh@outlook.com>
2021-05-05 03:12:57 +0530

    Expert mode switch and its fields, styling fixed.

--- embark/static/Scripts/main.js
+++ embark/static/Scripts/main.js
@@ -1,26 +1,37 @@
-function uploader(){
-$.ajax({
-    url:"upload/",
-    datatype:"html",
-    type: "GET",
-    success: function(data) {
-        console.log(data);
-        document.getElementById("uploader").innerHTML =
-        data;
-        // $('#uploader').html(data);
-    }
-  });}
-
-  function fwdetails(){
+function uploader() {
     $.ajax({
-        url:"firmwaredetails/",
-        datatype:"html",
+        url: "upload/",
+        datatype: "html",
         type: "GET",
-        success: function(data) {
+        success: function (data) {
             console.log(data);
-            document.getElementById("details").innerHTML =
-            data;
+            document.getElementById("uploader").innerHTML =
+                data;
+            // $('#uploader').html(data);
         }
-      });
+    });
+    // fwdetails();
+}
 
-  }
+// function fwdetails() {
+//     $.ajax({
+//         url: "firmwaredetails/",
+//         datatype: "html",
+//         type: "GET",
+//         success: function (data) {
+//             console.log(data);
+//             document.getElementById("details").innerHTML =
+//                 data;
+//         }
+//     });
+
+// }
+
+function expertModeOn() {
+    var x = document.getElementById("expertOptions");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
