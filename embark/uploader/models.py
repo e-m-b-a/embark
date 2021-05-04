29f7f59be8ca5cce6b393435ca8b125ddb10c9f5
Mani Kumar <mani.vanteru@fau.de>
2021-05-04 15:54:01 -0400

    Created a document to save the upload file and its data fields

--- embark/uploader/models.py
+++ embark/uploader/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from mongoengine import Document
+
+
+class Firmware(Document):
+    title = StringField(required=True, max_length=200)
+    posted = DateTimeField(default=datetime.datetime.utcnow)
+    url = StringField(required=True)
+    version = StringField(required=True, max_length=200)
+    vendor = StringField(required=True, max_length=200)
+    device = StringField(required=True, max_length=200)
+    notes = StringField(required=True, max_length=200)
+    firmware_Architecture = StringField(required=True, max_length=100)
+    cwe_checker = BooleanField(required=True, default=False)
+    docker_container = BooleanField(required=True, default=False)
+    log_path = BooleanField(required=True, default=False)
+    grep_able_log = BooleanField(required=True, default=False)
+    relative_paths = BooleanField(required=True, default=False)
+    ANSI_color = BooleanField(required=True, default=False)
+    web_reporter = BooleanField(required=True, default=False)
+    
+
+
 
 # Create your models here.
