c21d23594074c1e9ee0f4337dc3abda7ce9bf8b4
Maximilian Wagner <maximilian.b.wagner@fau.de>
2021-05-04 23:30:01 +0200

    Integrate unpacker in uploader. Works fast even for large files. Implement additional .gz unpack functionality

--- embark/uploader/unpacker.py
+++ embark/uploader/unpacker.py
@@ -1,4 +1,7 @@
+import gzip
 import logging
+import os
+import re
 import shutil
 
 
@@ -11,11 +14,31 @@ class unpacker:
     def __init__(self):
         # default formats: zip, tar, gztar, bztar, xztar
 
-        # register additional formats
-        # e.g. shutil.register_archive_format(name, function[, extra_args[, description]])
-
+        # register additional formats ( gz )
+        shutil.register_unpack_format('gz', ['.gz', ], self.gunzip_file)
         pass
 
+    @staticmethod
+    def gunzip_file(file_name, work_dir):
+        """
+            special unzip function for .gz files
+
+            :param file_name: file name of the gz file
+            :param work_dir: directory where the archive is located
+
+            :return:
+        """
+
+        # see warning about filename
+        filename = os.path.split(file_name)[-1]
+        filename = re.sub(r"\.gz$", "", filename, flags=re.IGNORECASE)
+
+        # extract .gz file
+        with gzip.open(file_name, 'rb') as f_in:
+            with open(os.path.join(work_dir, filename), 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+    # TODO: maybe make unique
     @staticmethod
     def unpack(file_location, extract_dir=None):
         """
@@ -36,11 +59,11 @@ class unpacker:
             return True
         except shutil.ReadError:
             logging.error("Format .%s is not supported", file_location.split(".", 1)[1])
-            return False
+            raise ValueError
         except Exception as ex:
             logging.error("Undefined Error during unpacking file: %s", file_location)
             logging.error(ex)
-            return False
+            raise ex
 
     @staticmethod
     def get_supported_formats():
@@ -50,4 +73,5 @@ class unpacker:
             :return: list of all supported formats for unpacking
         """
 
-        return [file_format for (file_format, description) in shutil.get_archive_formats()]
+        return [name for (name, extensions, description) in shutil.get_unpack_formats()]
+
