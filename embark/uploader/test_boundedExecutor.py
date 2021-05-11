c6ac7c8ece914c19e2a03c07dcce16d30e7aa949
Maximilian Wagner <wagnermaximilian@aol.com>
2021-05-11 15:36:20 +0200

    Fix test cases.

--- embark/uploader/test_boundedExecutor.py
+++ embark/uploader/test_boundedExecutor.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 
-from . import boundedExecutor
+from .boundedExecutor import boundedExecutor
 
 
 class test_boundedExecutor(TestCase):
 
     def setUp(self):
-        self.boundExecutor = boundedExecutor(bound=2, max_workers=2)
+        self.executor = boundedExecutor(bound=2, max_workers=2)
 
     # TODO: add timeout
     def test_non_blocking_overflow(self):
@@ -15,12 +15,12 @@ class test_boundedExecutor(TestCase):
 
         for _ in range(4):
             # if testing under windows use "timeout /T 5" instead of "sleep 5"
-            fut = self.boundExecutor.submit(self.boundExecutor.run_shell_cmd, "sleep 5")
+            fut = self.executor.submit(self.executor.run_shell_cmd, "sleep 5")
             self.assertIsNotNone(fut)
             fut_list.append(fut)
 
         for _ in range(2):
-            fut = self.boundExecutor.submit(self.boundExecutor.run_shell_cmd, "sleep 5")
+            fut = self.executor.submit(self.executor.run_shell_cmd, "sleep 5")
             self.assertIsNone(fut)
 
         for fut in fut_list:
