6733ce2369a7856a208a61eb6af1fd6a10a4c958
Mani Kumar <mani.vanteru@fau.de>
2021-05-11 07:50:14 -0400

    Fixed lint issues and requestes

--- embark/uploader/models.py
+++ embark/uploader/models.py
@@ -10,18 +10,18 @@ class Firmware(models.Model):
     vendor = models.TextField(help_text='Firmware vendor (double quote your input)')
     device = models.TextField(help_text='Device (double quote your input)')
     notes = models.TextField(help_text='Testing notes (double quote your input)')
-    firmware_Architecture = models.TextField(help_text=
-    'Architecture of the linux firmware [MIPS, ARM, x86, x64, PPC],-a will be added')
-    cwe_checker = models.BooleanField(help_text='Enables cwe-checker,-c will be added', default=False)
-    docker_container = models.BooleanField(help_text=
-    'Run emba in docker container, -D will be added', default=False)
-    deep_extraction = models.BooleanField(help_text=
-    'Enable deep extraction - try to extract every file two times with binwalk, -x will be added', default=False)
+    firmware_Architecture = models.TextField(help_text='Architecture of the linux firmware [MIPS, ARM, x86, x64, PPC]
+    ,-a will be added')
+    cwe_checker = models.BooleanField(help_text='Enables cwe-checker,-c will be added',
+    default=False)
+    docker_container = models.BooleanField(help_text='Run emba in docker container, -D will be added', default=False)
+    deep_extraction = models.BooleanField(help_text='Enable deep extraction - try to extract every file two times with binwalk, -x will be added',
+    default=False)
     log_path = models.BooleanField(help_text='Ignores log path check, -i will be added', default=False)
-    grep_able_log = models.BooleanField(help_text=
-    'Create grep-able log file in [log_path]/fw_grep.log, -g will be added', default=False)
-    relative_paths = models.BooleanField(help_text=
-    'Prints only relative paths, -s will be added', default=False)
+    grep_able_log = models.BooleanField(help_text='Create grep-able log file in [log_path]/fw_grep.log, -g will be added',
+    default=False)
+    relative_paths = models.BooleanField(help_text='Prints only relative paths, -s will be added',
+    default=False)
     ANSI_color = models.BooleanField(help_text='Adds ANSI color codes to log, -z will be added', default=False)
     web_reporter = models.BooleanField(help_text='Activates web report creation in log path, -W will be added', default=False)
     emulation_test = models.BooleanField(help_text='Enables automated qemu emulation tests (WARNING this module could harm your host!), -E will be added', default=False)
@@ -30,15 +30,15 @@ class Firmware(models.Model):
         path = f"{self.id}/{self.title}"
         command = f"{path}"
         if self.version:
-            command = command + "-X"
+            command = command + "-X" + str([self.version])
         if self.vendor:
-            command = command + "-Y"
+            command = command + "-Y" + str([self.vendor])
         if self.device:
-            command = command + "-Z"
+            command = command + "-Z" + str([self.device])
         if self.notes:
-            command = command + "-N"
+            command = command + "-N" + str([self.notes])
         if self.firmware_Architecture:
-            command = command + "-a"
+            command = command + "-a" + str([self.firmware_Architecture])
         if self.cwe_checker:
             command = command + " -c"
         if self.docker_container:
@@ -57,3 +57,4 @@ class Firmware(models.Model):
             command = command + "-W"
         if self.emulation_test:
             command = command + "-E"
+    return command
