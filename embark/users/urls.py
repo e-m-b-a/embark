00f2e030bcecbb8dcef1e692902d4680d73e8c83
VAISHNAVI UMESH <vaishnavi.umesh@fau.de>
2021-05-25 21:04:16 +0530

    urls and views users

--- embark/users/urls.py
+++ embark/users/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from users import views
+from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
+    path('', include('uploader.urls')),
     path('signin', views.signin, name='embark-signin'),
     path('signup', views.signup, name='embark-signup'),
 ]
