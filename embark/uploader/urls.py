a02eb1b6bea9c3265af65fec37027d7334225394
Ashutosh Singh <40604544+ashutoshsingh0223@users.noreply.github.com>
2021-06-09 11:46:54 +0200

    Lint fixes

--- embark/uploader/urls.py
+++ embark/uploader/urls.py
@@ -11,9 +11,7 @@ urlpatterns = [
     path('home/about/', views.about, name='embark-about'),
     path('home/upload/', views.upload_file, name='embark-File'),
     path('home/upload/save_file', views.save_file, name='embark-FileSave'),
-  
     path('home/serviceDashboard/', views.serviceDashboard, name='embark-ServiceDashboard'),
     path('home/reportDashboard/', views.reportDashboard, name='embark-ReportDashboard'),
-  
     path('download_zipped/<int:analyze_id>/', views.download_zipped, name='embark-download')
 ]
