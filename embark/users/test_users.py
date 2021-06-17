b6e74820615a853c51f7f6d5a7e7be72bc64ab4f
Garima Chauhan <ashutosh.singh.de@gmail.com>
2021-06-17 19:16:11 +0200

    Add cases for sigin failure

--- embark/users/test_users.py
+++ embark/users/test_users.py
@@ -5,17 +5,29 @@ from users.models import User
 
 class TestUsers(TestCase):
     def setUp(self) -> None:
-        pass
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        self.client = Client()
 
     def test_signup(self):
         pass
 
     def test_signin(self):
-        user = User.objects.create(username='testuser')
-        user.set_password('12345')
-        user.save()
+        # Right password would redirect to home
+        response = self.client.post('/signin', {'email': 'testuser', 'password': '12345'})
+        self.assertEqual(response.status_code, 302)
 
-        c = Client()
+    def test_signin_wrong_password(self):
+        # Right password would render the same page again with 200 status code
+        response = self.client.post('/signin', {'email': 'testuser', 'password': '1234'})
+        self.assertEqual(response.status_code, 200)
 
-        response = c.post('/login/', {'username': 'john', 'password': 'smith'})
-        print(response.status_code)
+    def test_signin_wrong_body(self):
+        # Signin api expects two keys in the body.
+        # email and password. On getting wrong body.
+        # it throws 200 status code with content: ""User data is invalid""
+        response = self.client.post('/signin', {'username': 'testuser', 'password': '12345'})
+        self.assertEqual(response.status_code, 200)
+        print(response.content)
+        self.assertEqual(response.content, "User data is invalid")
