8b58497dc7d8a96eca29b7b3eb229f81e34cb3a2
Garima Chauhan <ashutosh.singh.de@gmail.com>
2021-06-17 19:22:03 +0200

    Finalize signin test cases

--- embark/users/test_users.py
+++ embark/users/test_users.py
@@ -14,20 +14,33 @@ class TestUsers(TestCase):
         pass
 
     def test_signin(self):
-        # Right password would redirect to home
+        """
+        Right password would redirect to home
+        Returns:
+
+        """
         response = self.client.post('/signin', {'email': 'testuser', 'password': '12345'})
         self.assertEqual(response.status_code, 302)
 
     def test_signin_wrong_password(self):
-        # Right password would render the same page again with 200 status code
+        """
+        Right password would render the same page again with 200 status code
+        Returns:
+
+        """
         response = self.client.post('/signin', {'email': 'testuser', 'password': '1234'})
         self.assertEqual(response.status_code, 200)
 
     def test_signin_wrong_body(self):
-        # Signin api expects two keys in the body.
-        # email and password. On getting wrong body.
-        # it throws 200 status code with content: ""User data is invalid""
+        """
+        Signin api expects two keys in the body.
+        email and password. On getting wrong body.
+        it throws 200 status code with content: ""User data is invalid""
+
+        Returns:
+
+        """
+
         response = self.client.post('/signin', {'username': 'testuser', 'password': '12345'})
         self.assertEqual(response.status_code, 200)
-        print(response.content)
-        self.assertEqual(response.content, "User data is invalid")
+        self.assertEqual(response.content.decode("utf-8"), "User data is invalid")
