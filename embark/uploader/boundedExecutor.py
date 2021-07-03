4984cbea25ede73dd672a06b657c9204595a0b45
Maximilian Wagner <wagnermaximilian@aol.com>
2021-07-03 12:51:07 +0200

    Fix missing directroy bug. emba cant create 2 missing directories.

--- embark/uploader/boundedExecutor.py
+++ embark/uploader/boundedExecutor.py
@@ -140,6 +140,8 @@ class BoundedExecutor:
 
         # evaluate meta information and safely create log dir
         emba_log_location = f"/app/emba/{settings.LOG_ROOT}/{firmware_flags.pk}"
+        log_path = Path(emba_log_location)
+        log_path.mkdir(parents=True, exist_ok=True)
 
         firmware_flags.path_to_logs = emba_log_location
         firmware_flags.save()
