2175a6884286a0dcf1fd28947832b604bb4cd7a9
m-1-k-3 <michael.messner@siemens-energy.com>
2021-08-11 11:36:34 +0200

    test

--- embark/uploader/test_models.py
+++ embark/uploader/test_models.py
@@ -34,7 +34,7 @@ class test_models(TestCase):
         firmware.dependency_check = True
         firmware.multi_threaded = True
 
-        expected_string = " -X version -Y vendor -Z device -N notes -a x64 -c -x -i -g -s -z -W -E -F -t"
+        expected_string = " -X version -Y vendor -Z device -N notes -a x64 -c -x -i -g -s -z -W -E -F -t -r"
         self.assertEqual(firmware.get_flags(), expected_string)
 
     def test_get_flags_all_false(self):
