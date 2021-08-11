38ed191a626f9df96a9af8acd6f74924e51467ba
m-1-k-3 <michael.messner@siemens-energy.com>
2021-08-11 15:06:44 +0200

    header

--- installer.sh
+++ installer.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# EMBArk - The firmware security scanning environment
+#
+# Copyright 2020-2021 Siemens Energy AG
+# Copyright 2020-2021 Siemens AG
+#
+# EMBArk comes with ABSOLUTELY NO WARRANTY.
+#
+# EMBArk is licensed under MIT
+#
+# Author(s): Michael Messner, Pascal Eckmann
+
+# Description: Installer for EMBArk
+
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BOLD='\033[1m'
+NC='\033[0m' # no color
+
