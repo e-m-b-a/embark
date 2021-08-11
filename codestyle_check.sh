d0fe849ec3c5472b0eb77be9c21636ec3e610983
m-1-k-3 <michael.messner@siemens-energy.com>
2021-08-11 13:43:20 +0200

    codestyle checker

--- codestyle_check.sh
+++ codestyle_check.sh
@@ -1,14 +1,83 @@
 #!/bin/bash
 
-echo -e "[*] Searching shell scripts and test with shellcheck"
-shellcheck codestyle_check.sh
-find embark -iname "*.sh" -exec shellcheck {} \;
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BOLD='\033[1m'
+NC='\033[0m' # no color
 
-echo -e "[*] Searching python files and test with pycodestyle.py"
-find embark -name "*.py" -exec python3 /usr/lib/python3/dist-packages/pycodestyle.py --first {} \;
+shellchecker() {
+  echo -e "\\n""$ORANGE""$BOLD""EMBArk Shellcheck""$NC""\\n""$BOLD""=================================================================""$NC"
+  if ! command -v shellcheck >/dev/null 2>&1; then
+    echo -e "\\n""$ORANGE""Shellcheck not found!""$NC""\\n""$ORANGE""Install shellcheck via 'apt-get install shellcheck'!""$NC\\n"
+    exit 1
+  fi
 
-echo -e "[*] Test project with pytest"
-pytest
+  echo -e "\\n""$GREEN""Run shellcheck on installer:""$NC""\\n"
+  if shellcheck ./installer.sh || [[ $? -ne 1 && $? -ne 2 ]]; then
+    echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
+  else
+    echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
+  fi
 
-#echo -e "[*] Searching python files and test with pylint:"
-#pylint --max-line-length=240 embark/*
+  echo -e "\\n""$GREEN""Run shellcheck on this script:""$NC""\\n"
+  if shellcheck ./codestyle_check.sh || [[ $? -ne 1 && $? -ne 2 ]]; then
+    echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
+  else
+    echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
+  fi
+
+  echo -e "\\n""$GREEN""Find shell scripts and run shellcheck on them:""$NC""\\n"
+  mapfile -t SH_SCRIPTS < <(find embark -iname "*.sh")
+  for SH_SCRIPT in "${SH_SCRIPTS[@]}"; do
+    echo -e "\\n""$GREEN""Run shellcheck on $SH_SCRIPT:""$NC""\\n"
+    if shellcheck "$SH_SCRIPT" || [[ $? -ne 1 && $? -ne 2 ]]; then
+      echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
+    else
+      echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
+    fi
+  done
+}
+
+pycodestyle_check(){
+  echo -e "\\n""$ORANGE""$BOLD""EMBArk pycodestyle check""$NC""\\n""$BOLD""=================================================================""$NC"
+  echo -e "[*] Searching python files and test with pycodestyle.py"
+  if command -v pycodstyle.py >/dev/null 2>&1; then
+    echo -e "\\n""$ORANGE""pycodestyle found in PATH!""$NC""\\n"
+    PYCODESTYLE="pycodestyle.py"
+  elif [[ -f "/usr/lib/python3/dist-packages/pycodestyle.py" ]]; then
+    echo -e "\\n""$ORANGE""pycodestyle found!""$NC""\\n"
+    PYCODESTYLE="/usr/lib/python3/dist-packages/pycodestyle.py"
+  else
+    echo -e "\\n""$ORANGE""pycodestyle not found!""$NC""\\n""$ORANGE""Install shellcheck via 'apt-get install shellcheck'!""$NC\\n"
+    exit 1
+  fi
+
+  echo -e "\\n""$GREEN""Find python scripts and run pycodestyle on them:""$NC""\\n"
+  mapfile -t PY_SCRIPTS < <(find embark -iname "*.py")
+  for PY_SCRIPT in "${PY_SCRIPTS[@]}"; do
+    echo -e "\\n""$GREEN""Run pycodestyle on $PY_SCRIPT:""$NC""\\n"
+    if python3 "$PYCODESTYLE" --first "$PY_SCRIPT" || [[ $? -ne 1 && $? -ne 2 ]]; then
+      echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
+    else
+      echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
+    fi
+  done
+}
+
+pytester(){
+  echo -e "[*] Test project with pytest"
+  pytest
+}
+
+pylinter(){
+  echo -e "[*] Searching python files and test with pylint (not supported)"
+  #pylint --max-line-length=240 embark/*
+}
+
+shellchecker
+pycodestyle_check
+pytester
+pylinter
+
+
+
