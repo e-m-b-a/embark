248a8ca253238de5fc230a0b31cac30807a55cff
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-11-24 11:23:03 +0000

    installs mv to dev

--- installer.sh
+++ installer.sh
@@ -213,14 +213,6 @@ install_debs() {
   if ! command -v docker-compose > /dev/null ; then
     apt-get install -y -q docker-compose
   fi
-  #TODO put all these into dev
-  if ! command -v pycodestyle > /dev/null ; then 
-    apt-get install -y -q pycodestyle
-  fi
-  if ! command -v pylint > /dev/null ; then 
-    apt-get install -y -q pylint
-    apt-get install -y -q python3-pylint-django
-  fi
   # we need the django package on the host for generating the django SECRET_KEY and pip
   apt-get install -y -q python3-django python3-pip
 }
@@ -229,7 +221,7 @@ install_debs() {
 make_dev_env(){
   echo -e "\n$GREEN""$BOLD""Building Developent-Enviroment for EMBArk""$NC"
   install_debs
-  apt-get install -y -q python3-dev default-libmysqlclient-dev build-essential sqlite3 pipenv npm
+  apt-get install -y -q python3-dev default-libmysqlclient-dev build-essential sqlite3 pipenv npm pycodestyle pylint python3-pylint-django
   npm install -g jshint # global install
   pipenv install --dev
   # download externals
