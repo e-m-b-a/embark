672b6479c1a7e84db7fbc5219695977c29b3e0b1
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-12-03 08:56:17 +0000

    ch .env generation

--- embark/embark/settings.py
+++ embark/embark/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')   # defined in compose or dev-start.sh
+SECRET_KEY = os.environ.get('SECRET_KEY')   # look at .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
