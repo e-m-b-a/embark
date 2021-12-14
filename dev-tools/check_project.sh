203887a61dc00ee9c334bc55844b9e81eff9cb1a
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-12-14 10:49:11 +0000

    ch no root + add docker check

--- dev-tools/check_project.sh
+++ dev-tools/check_project.sh
@@ -16,6 +16,7 @@
 #               And check Django if its deployable
 
 export DJANGO_SETTINGS_MODULE=embark.settings
+export PYTHONPATH="$PYTHONPATH:${PWD}/embark/embark/"
 
 cd "$(dirname "$0")" || exit 1
 cd .. || exit 1 
@@ -25,6 +26,10 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BOLD='\033[1m'
 NC='\033[0m' # no color
+
+if [[ $EUID -eq 0 ]]; then
+  echo -e "\\n$RED""Running this script as root is not supported""$NC\\n"
+fi
 
 # check that all tools are installed
 check_tools(){
@@ -43,7 +48,9 @@ check_tools(){
 
 # checks django configuration
 check_django(){
-  pipenv run ./embark/manage.py check --deploy
+  cd ./embark || exit 1
+  pipenv run python ./embark/manage.py check --deploy
+  cd .. || exit 1
 }
 
 # checks js-scripts with jshint for errors
@@ -53,7 +60,7 @@ jscheck(){
   for JS_SCRIPT in "${JS_SCRIPTS[@]}"; do
     echo -e "\\n""$GREEN""Run jshint on $JS_SCRIPT:""$NC""\\n"
     # mapfile -t JS_RESULT < <(jshint "$JS_SCRIPT")
-    jshint "$JS_SCRIPT" >"$JS_SCRIPT.report"
+    jshint "$JS_SCRIPT" >/dev/null
     RES=$?
     if [[ $RES -eq 2 ]] ; then
       echo -e "\\n""$RED$BOLD==> FIX ERRORS""$NC""\\n"
@@ -172,10 +179,10 @@ banditer() {
 
   for PY_SCRIPT in "${PY_SCRIPTS[@]}"; do
     echo -e "\\n""$GREEN""Run bandit on $PY_SCRIPT:""$NC""\\n"
-    if bandit "$PY_SCRIPT" 2> /dev/null | grep -q "No issues identified."; then
+    if bandit -c .banditrc "$PY_SCRIPT" 2> /dev/null | grep -q "No issues identified."; then
       echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
     else
-      bandit "$PY_SCRIPT"
+      bandit -c .banditrc "$PY_SCRIPT"
       echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
       ((MODULES_TO_CHECK=MODULES_TO_CHECK+1))
       MODULES_TO_CHECK_ARR+=( "$PY_SCRIPT" )
@@ -185,11 +192,12 @@ banditer() {
 }
 
 pylinter(){
+  cd ./embark || exit 1
   echo -e "\\n""$ORANGE""$BOLD""EMBArk pylint check""$NC""\\n""$BOLD""=================================================================""$NC"
-  mapfile -t PY_SCRIPTS < <(find embark -type d -name migrations -prune -false -o -iname "*.py")
+  mapfile -t PY_SCRIPTS < <(find . -type d -name migrations -prune -false -o -iname "*.py")
   for PY_SCRIPT in "${PY_SCRIPTS[@]}"; do
     echo -e "\\n""$GREEN""Run pylint on $PY_SCRIPT:""$NC""\\n"
-    mapfile -t PY_RESULT < <(pipenv run pylint "$PY_SCRIPT")
+    mapfile -t PY_RESULT < <(pipenv run pylint --rcfile=../.pylintrc "$PY_SCRIPT")
     local RATING_10=0
     if [[ "${#PY_RESULT[@]}" -gt 0 ]]; then 
       if ! printf '%s\n' "${PY_RESULT[@]}" | grep -q -P '^Your code has been rated at 10'; then
@@ -212,9 +220,25 @@ pylinter(){
   done
 
   echo -e "\\n""$GREEN""Run pylint on all scripts:""$NC""\\n"
-  pipenv run pylint --max-line-length=240 -d C0115,C0114,C0116,W0511,W0703 --load-plugins pylint_django embark/* | grep "Your code has been rated"
+  pipenv run pylint --rcfile=../.pylintrc ./* | grep "Your code has been rated"
   # current rating: 9.52/10
   # start rating: 5.58/10
+  cd .. || exit 1
+}
+
+dockerchecker(){
+  echo -e "\\n""$ORANGE""$BOLD""EMBArk docker-files check""$NC""\\n""$BOLD""=================================================================""$NC"
+  mapfile -t DOCKER_COMPS < <(find .  -maxdepth 1 -type d -name migrations -prune -false -o -iname "docker-compose*.yml")
+  for DOCKER_COMP in "${DOCKER_COMPS[@]}"; do
+    if ! docker-compose -f "$DOCKER_COMP" config 1>/dev/null; then
+      echo -e "$GREEN""$BOLD""==> SUCCESS""$NC""\\n"
+    else
+      echo -e "\\n""$ORANGE$BOLD==> FIX ERRORS""$NC""\\n"
+      ((MODULES_TO_CHECK=MODULES_TO_CHECK+1))
+      MODULES_TO_CHECK_ARR+=( "$DOCKER_COMP" )    
+    fi
+  done
+  #TODO dockerfile itself
 }
 
 #main
@@ -222,14 +246,13 @@ check_tools
 MODULES_TO_CHECK=0
 MODULES_TO_CHECK_ARR=()
 shellchecker
+dockerchecker
 jscheck
 templatechecker
 pycodestyle_check
 banditer
 pylinter
 #check_django TODO
-
-
 
 if [[ "${#MODULES_TO_CHECK_ARR[@]}" -gt 0 ]]; then
   echo -e "\\n\\n""$GREEN$BOLD""SUMMARY:$NC\\n"
