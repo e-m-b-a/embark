6e2159e91733326cc593a2c180c40b44ae5d0c15
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-12-14 12:00:21 +0000

    ch var

--- embark/uploader/test_boundedexecutor.py
+++ embark/uploader/test_boundedexecutor.py
@@ -13,13 +13,13 @@ class TestBoundedExecutor(TestCase):
 
         fut_list = []
 
-        for _ in range(MAX_WORKERS + MAX_QUEUE):
+        for x in range(MAX_WORKERS + MAX_QUEUE):
             # if testing under windows use "timeout /T 5" instead of "sleep 5"
             fut = BoundedExecutor.submit(BoundedExecutor.run_emba_cmd, "sleep 5")
             self.assertIsNotNone(fut)
             fut_list.append(fut)
 
-        for _ in range(MAX_WORKERS):
+        for x in range(MAX_WORKERS):
             fut = BoundedExecutor.submit(BoundedExecutor.run_emba_cmd, "sleep 5")
             self.assertIsNone(fut)
 
