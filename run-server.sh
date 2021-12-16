4392e5d9e6e7360d981bd1823e0838d84643cce6
BenediktMKuehne <bennimail.4spam@gmail.com>
2021-12-16 13:38:14 +0000

    fix?

--- run-server.sh
+++ run-server.sh
@@ -77,7 +77,8 @@ pipenv run ./embark/manage.py runmodwsgi --setup-only --port=80 --user www-data 
 /app/mod_wsgi-express-80/apachectl start
 
 echo -e "\n[""$BLUE JOB""$NC""] Starting daphne(ASGI) - log to /embark/logs/daphne.log"
-pipenv run daphne -v 3 --access-log ./embark/logs/daphne.log -p 8001 -b '0.0.0.0' --root-path="$PWD/embark" embark.asgi:application 1>/dev/null
-
+cd ./embark || exit 1
+pipenv run daphne -v 3 --access-log ./embark/logs/daphne.log -p 8001 -b '0.0.0.0' --root-path="$PWD" embark.asgi:application 1>/dev/null
+cd .. || exit 1
 wait %1
 wait %2
