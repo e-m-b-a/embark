fde6f0e95531bd4b9bcbdec9a186a850b250651d
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-01-12 08:45:13 +0000

    ch deps

--- installer.sh
+++ installer.sh
@@ -128,7 +128,7 @@ install_debs() {
 install_embark_default() {
   echo -e "\n$GREEN""$BOLD""Installation of the firmware scanning environment EMBArk""$NC"
   install_debs
-
+  apt-get install -y -q python3-dev default-libmysqlclient-dev build-essential pipenv   # TODO ?*-dev needed? try
   #Add user for server
   useradd www-embark -G sudo -c "embark-server-user" -M -r --shell=/usr/sbin/nologin -d /app/www/
   echo 'www-embark ALL=(ALL) NOPASSWD: /app/emba/emba.sh' | EDITOR='tee -a' visudo
@@ -235,8 +235,11 @@ install_embark_default() {
   echo -e "$GREEN""$BOLD""Which starts the server on (0.0.0.0) port 80 ""$NC"
 }
 
-#install as container TODO!!!!!!!!!
+#install as docker-service
 install_embark_docker(){
+  echo -e "\n$GREEN""$BOLD""Installing EMBArk as docker-container""$NC"
+  install_debs
+
   echo -e "\n$GREEN""$BOLD""Downloading of external files, e.g. jQuery, for the offline usability of EMBArk""$NC"
   mkdir -p ./embark/static/external/{scripts,css}
   wget -O ./embark/static/external/scripts/jquery.js https://code.jquery.com/jquery-3.6.0.min.js
@@ -251,45 +254,56 @@ install_embark_docker(){
 
   # generating dynamic authentication for backend
   # for MYSQL root pwd check the logs of the container
-  if ! [[ -f .env ]]; then
-    DJANGO_SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
-    PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 )
-    echo -e "$ORANGE""$BOLD""Creating a default EMBArk configuration file .env""$NC"
-    export DATABASE_NAME="embark"
-    export DATABASE_USER="embark"
-    export DATABASE_PASSWORD="$PASSWORD"
-    export DATABASE_HOST="127.0.0.1"
-    export DATABASE_PORT="3306"
-    export MYSQL_PASSWORD="$PASSWORD"
-    export MYSQL_USER="embark"
-    export MYSQL_DATABASE="embark"
-    export REDIS_HOST="127.0.0.1"
-    export REDIS_PORT="7777"
-    export SECRET_KEY="$DJANGO_SECRET_KEY"
-    # this is for pipenv/django # TODO change/lock after deploy
-    {
-      echo "DATABASE_NAME=$DATABASE_NAME"
-      echo "DATABASE_USER=$DATABASE_USER" 
-      echo "DATABASE_PASSWORD=$DATABASE_PASSWORD"
-      echo "DATABASE_HOST=$DATABASE_HOST"
-      echo "DATABASE_PORT=$DATABASE_PORT"
-      echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
-      echo "MYSQL_USER=$MYSQL_USER"
-      echo "MYSQL_DATABASE=$MYSQL_DATABASE"
-      echo "REDIS_HOST=$REDIS_HOST"
-      echo "REDIS_PORT=$REDIS_PORT"
-      echo "SECRET_KEY=$DJANGO_SECRET_KEY"
-      echo "PYTHONPATH=${PYTHONPATH}:${PWD}:${PWD}/embark/"
-    } > .env
-    echo -e "$RED""$BOLD""WARNING: The default EMBArk configuration includes a key & password generation via the shell script!""$NC"
-    cat .env
+  DJANGO_SECRET_KEY=$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+  PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 )
+  echo -e "$ORANGE""$BOLD""Creating a container EMBArk configuration file .env""$NC"
+  export DATABASE_NAME="embark"
+  export DATABASE_USER="embark"
+  export DATABASE_PASSWORD="$PASSWORD"
+  export DATABASE_HOST="127.0.0.1"
+  export DATABASE_PORT="3306"
+  export MYSQL_PASSWORD="$PASSWORD"
+  export MYSQL_USER="embark"
+  export MYSQL_DATABASE="embark"
+  export REDIS_HOST="127.0.0.1"
+  export REDIS_PORT="7777"
+  export SECRET_KEY="$DJANGO_SECRET_KEY"
+  # this is for pipenv/django # TODO change/lock after deploy
+  {
+    echo "DATABASE_NAME=$DATABASE_NAME"
+    echo "DATABASE_USER=$DATABASE_USER" 
+    echo "DATABASE_PASSWORD=$DATABASE_PASSWORD"
+    echo "DATABASE_HOST=$DATABASE_HOST"
+    echo "DATABASE_PORT=$DATABASE_PORT"
+    echo "MYSQL_PASSWORD=$MYSQL_PASSWORD"
+    echo "MYSQL_USER=$MYSQL_USER"
+    echo "MYSQL_DATABASE=$MYSQL_DATABASE"
+    echo "REDIS_HOST=$REDIS_HOST"
+    echo "REDIS_PORT=$REDIS_PORT"
+    echo "SECRET_KEY=$DJANGO_SECRET_KEY"
+    echo "PYTHONPATH=${PYTHONPATH}:${PWD}:${PWD}/embark/"
+  } > .env
+
+  # get emba
+  if ! [[ -d ./emba ]]; then
+    git clone https://github.com/e-m-b-a/emba.git
   else
-    echo -e "$GREEN""$BOLD""Using the provided EMBArk configuration file .env""$NC"
-    cat .env
+    cd emba || exit 1
+    RES=$(git pull;)
+    cd .. || exit 1
   fi
 
+  # install on host 
+  if ! [[ "$RES" == "Already up to date." ]]; then
+    cd emba || exit 1
+    ./installer.sh -d
+    cp ./config/emba_updater /etc/cron.daily/
+    cd .. || exit 1
+  fi
+
+  # setup dbs-container and detach build could be skipt
   echo -e "\n$GREEN""$BOLD""Building EMBArk docker images""$NC"
-  docker-compose build
+  docker-compose -f ./docker-compose-docker.yml build
   DB_RETURN=$?
   if [[ $DB_RETURN -eq 0 ]] ; then
     echo -e "$GREEN""$BOLD""Finished building EMBArk docker images""$NC"
@@ -297,41 +311,30 @@ install_embark_docker(){
     echo -e "$ORANGE""$BOLD""Failed building EMBArk docker images""$NC"
   fi
 
-  echo -e "\n$GREEN""$BOLD""Setup mysql and redis docker images""$NC"
-  docker-compose up -d
-  DU_RETURN=$?
-  if [[ $DU_RETURN -eq 0 ]] ; then
-    echo -e "$GREEN""$BOLD""Finished setup mysql and redis docker images""$NC"
-  else
-    echo -e "$ORANGE""$BOLD""Failed setup mysql and redis docker images""$NC"
-  fi
-  
-  # TODO compose work without restart
-  echo -e "\n$GREEN""$BOLD""Restarting EMBArk docker images""$NC"
-  docker-compose restart embark
+  echo -e "\n$GREEN""$BOLD""Starting EMBArk docker images""$NC"
+  docker-compose -f ./docker-compose-docker.yml up -d
   DS_RETURN=$?
   if [[ $DS_RETURN -eq 0 ]] ; then
-    echo -e "$GREEN""$BOLD""Finished restarting EMBArk docker images""$NC"
+    echo -e "$GREEN""$BOLD""Finished starting EMBArk""$NC"
   else
-    echo -e "$ORANGE""$BOLD""Failed restarting EMBArk docker images""$NC"
+    echo -e "$ORANGE""$BOLD""Failed starting EMBArk""$NC"
   fi
 
   echo -e "$GREEN""$BOLD""Testing EMBArk installation""$NC"
   # need to wait a few seconds until everyting is up and running
   sleep 5
-  if curl -XGET 'http://0.0.0.0:80' | grep -q embark; then
-    echo -e "$GREEN""$BOLD""Finished installing EMBArk""$NC"
-  else
+  if ! curl -XGET 'http://0.0.0.0:80' | grep -q embark; then
     echo -e "$ORANGE""$BOLD""Failed installing EMBArk - check the output from the installation process""$NC"
   fi
 
-  echo -e "$GREEN""$BOLD""Ready to use @ localhost:80""$NC"
+  echo -e "$GREEN""$BOLD""Server ready to use""$NC"
+  echo -e "$GREEN""EMBArk is on (0.0.0.0) port 80 ""$NC"
 }
 
 install_embark_dev(){
   echo -e "\n$GREEN""$BOLD""Building Developent-Enviroment for EMBArk""$NC"
   install_debs
-  apt-get install -y -q python3-dev default-libmysqlclient-dev build-essential sqlite3 pipenv npm pycodestyle python3-pylint-django
+  apt-get install -y -q npm pycodestyle python3-pylint-django
   npm install -g jshint dockerlinter
   PIPENV_VENV_IN_PROJECT=1 pipenv install --dev
   # download externals
