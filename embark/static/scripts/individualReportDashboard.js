e502b7aeb8fba1cd549616e6432a430420b56b20
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-04-04 06:42:50 -0400

    ch scan_time to min

--- embark/static/scripts/individualReportDashboard.js
+++ embark/static/scripts/individualReportDashboard.js
@@ -188,7 +188,7 @@ get_individual_report().then(function (returnData) {
         "Firmware ID": returnData.name.replace(/\/.*$/,""),
         "Start date": returnData.start_date.replace('T', ' - '),
         "End date": returnData.end_date.replace('T', ' - '),
-        "Scan time": returnData.scan_time,
+        "Scan time": returnData.scan_time.minutes,
         "Vendor": returnData.vendor,
         "Version": returnData.version,
         "Notes": returnData.notes,
