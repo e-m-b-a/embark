e6d4f154a8648fdb575783d923a346afb84b2a05
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-04-04 07:13:44 -0400

    update

--- embark/uploader/boundedexecutor.py
+++ embark/uploader/boundedexecutor.py
@@ -95,6 +95,7 @@ class BoundedExecutor:
                 firmware = Firmware.objects.get(pk=primary_key)
                 firmware.end_date = datetime.now()
                 firmware.scan_time = firmware.start_date - datetime.now()
+                firmware.duration = firmware.scan_time.str
                 firmware.finished = True
                 firmware.save()
 
--- embark/uploader/models.py
+++ embark/uploader/models.py
@@ -187,6 +187,7 @@ class Firmware(models.Model):
     start_date = models.DateTimeField(default=datetime.now, blank=True)
     end_date = models.DateTimeField(default=datetime.min, blank=True)
     scan_time = models.DurationField(default=timedelta(), blank=True)
+    duration = models.TextField(default="Error", blank=True)
     finished = models.BooleanField(default=False, blank=False)
     failed = models.BooleanField(default=False, blank=False)
 
