eaf2a07bd22544bef372f39663b30d2c747f0e2f
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-04-26 04:53:28 -0400

    rm spaces

--- embark/uploader/urls.py
+++ embark/uploader/urls.py
@@ -4,7 +4,7 @@ from uploader import views
 
 # view routing
 urlpatterns = [
-    
+
     path('uploader/', views.uploader_home, name='embark-uploader-home'),
     path('uploader/save/', views.save_file, name='embark-uploader-save'),
     path('uploader/start/', views.start_analysis, name='embark-uploader-start-analysis'),
