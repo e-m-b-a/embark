1f6314e2189ba771301118b9fc96de8b99f9ce1a
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-04-27 05:42:23 -0400

    edit delete

--- embark/uploader/urls.py
+++ embark/uploader/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 
     path('uploader/', views.uploader_home, name='embark-uploader-home'),
     path('uploader/save/', views.save_file, name='embark-uploader-save'),
+    path('uploader/delete/', views.delete_fw_file, name='embark-uploader-delete'),
     path('uploader/start/', views.start_analysis, name='embark-uploader-start-analysis'),
     path('uploader/stop/', views.stop_analysis, name='embark-uploader-stop-analysis')
 ]
