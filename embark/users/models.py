09bc88b5dba613c44fcf052db9a1e53ca773c769
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-05-10 02:59:17 -0400

    fix

--- embark/users/models.py
+++ embark/users/models.py
@@ -30,7 +30,7 @@ class User(AbstractUser):
 class TeamMember(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team_member')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_team_member')
-    role = models.IntegerField(choices=Role.choices(), default=Role.viewer)
+    role = models.IntegerField(choices=Role.choices(), default=Role.Viewer)
     is_active = models.BooleanField(default=True, help_text='Whether this team member is active or not')
     created = models.DateTimeField(auto_now_add=True, help_text='Date time when this entry was created')
     modified = models.DateTimeField(auto_now=True, help_text='Date time when this entry was modified')
