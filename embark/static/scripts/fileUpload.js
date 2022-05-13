47bbb3406353d474f005927fba165e4e9efc4cb7
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-05-13 04:14:43 -0400

    fix

--- embark/static/scripts/fileUpload.js
+++ embark/static/scripts/fileUpload.js
@@ -32,7 +32,7 @@ $(window).bind("load", function() {
         document.querySelector("#file-input").onchange = function(){
             var fileNames = "";
             for (var i = 0; i < this.files.length; i++) {
-                fileNames = fileNames + this.value + "<br>";
+                fileNames = fileNames + this.value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + "<br>";
             }
             var target = document.querySelector("#file-name");
             target.innerHTML = fileNames;
