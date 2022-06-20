e793f7fd6f4fb588d615da46884ef33705ac8c24
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-06-20 07:06:50 +0200

    comment

--- installer.sh
+++ installer.sh
@@ -216,9 +216,10 @@ install_debs() {
     apt-get install -y -q docker-compose
   fi
   # we need the django package on the host for generating the django SECRET_KEY and pip
-  apt-get install -y -q python3-django python3-pip python3-dev
-
-  git config --global --add safe.directory "$PWD"
+  apt-get install -y -q python3-django python3-pip python3.9-dev
+  # python3-dev
+  # mark dir as safe for git
+  # git config --global --add safe.directory "$PWD"
 }
 
 install_daemon() {
