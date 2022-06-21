3d442cad21d536a58081f7ba20d93192d058b2c6
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-06-21 10:37:58 +0200

    ch filter

--- embark/dashboard/views.py
+++ embark/dashboard/views.py
@@ -85,7 +85,7 @@ def report_dashboard(request):
 
     :return: rendered ReportDashboard
     """
-    finished_firmwares = FirmwareAnalysis.objects.filter(finished=True).exclude(failed=True)
+    finished_firmwares = FirmwareAnalysis.objects.exclude(failed=True)
     return render(request, 'dashboard/reportDashboard.html', {'finished_firmwares': finished_firmwares, 'username': request.user.username})
 
 
