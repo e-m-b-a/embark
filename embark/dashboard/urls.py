ac8664562abe5a31169adca3ee2fc82142f5d795
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-09-30 13:32:35 +0200

    rename lookup name

--- embark/dashboard/urls.py
+++ embark/dashboard/urls.py
@@ -6,7 +6,7 @@ from dashboard import views
 # view routing
 urlpatterns = [
     path('dashboard/main/', views.main_dashboard, name='embark-MainDashboard'),
-    path('dashboard/service/', views.service_dashboard, name='embark-ServiceDashboard'),
+    path('dashboard/service/', views.service_dashboard, name='embark-dashboard-service'),
     path('dashboard/report/', views.report_dashboard, name='embark-ReportDashboard'),
     path('dashboard/individualReport/<uuid:analysis_id>', views.individual_report_dashboard, name='embark-IndividualReportDashboard'),
     path('dashboard/stop/', views.stop_analysis, name='embark-stop-analysis')
