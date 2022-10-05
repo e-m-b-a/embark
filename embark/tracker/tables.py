7db43ac96411bd812afbae08951ffca68ff388ca
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-10-05 12:15:41 +0200

    add link to table

--- embark/tracker/tables.py
+++ embark/tracker/tables.py
@@ -1,4 +1,6 @@
+from django.urls import reverse
 import django_tables2 as tables
+from django.utils.html import format_html
 from uploader.models import Device
 
 
@@ -6,3 +8,9 @@ class SimpleDeviceTable(tables.Table):
 
     class Meta:
         model = Device
+        attr = {
+
+        }
+
+    def render_id(self, value):
+        return format_html("<a href=\"%s\">%s</a>" % (reverse(viewname='embark-tracker-device', args=[value]), value))
