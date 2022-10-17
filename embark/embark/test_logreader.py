0416e5004b9f269148cc464bcc0b89df7f596a77
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-10-17 15:04:38 +0200

    fix testcase

--- embark/embark/test_logreader.py
+++ embark/embark/test_logreader.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from django.test import TestCase
 from uploader.models import FirmwareAnalysis
 
-from embark.logreader import EMBA_P_PHASE, EMBA_S_PHASE, PROCESS_MAP, LogReader
+from embark.logreader import EMBA_F_PHASE, EMBA_L_PHASE, EMBA_P_PHASE, EMBA_S_PHASE, LogReader
 
 logger = logging.getLogger(__name__)
 
@@ -22,28 +22,30 @@ class TestLogreader(TestCase):
         analysis.save()   # args??
 
         self.analysis_id = analysis.id
-        self.test_file = os.path.join(settings.BASE_DIR.parent, "test/logreader/test-run.log")
+        self.test_file_good = os.path.join(settings.BASE_DIR.parent, "test/logreader/test-run-good.log")
+        self.test_file_bad = os.path.join(settings.BASE_DIR.parent, "test/logreader/test-run-bad.log")
         # check test_log file
-        if not self.test_file.isfile():
-            return logging.ERROR
+        if not self.test_file_good.isfile() or not self.test_file_bad.isfile():
+            logger.error("test_files not accessable")
+            print("Files for testing not found")
 
-    def test_in_pro(self):
+    def file_test(self, file):
         logr = LogReader(self.analysis_id)
-        global PROCESS_MAP
-        with open(self.test_file, 'r', encoding='UTF-8') as test_file:
+        # global PROCESS_MAP
+        with open(file, 'r', encoding='UTF-8') as test_file:
             for line in test_file:
                 self.write_to_log(line)   # simulates EMBA
                 time.sleep(0.1)
                 # message check
-                print("PROCESSMAP with index %s looks like this:%s", str(self.analysis_id), PROCESS_MAP[str(self.analysis_id)])
-                self.assertTrue(filecmp(f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/logreader.log", f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/emba_logs/emba.log"), "Files not equal?!")   # check correctness of regexes
+                # print("PROCESSMAP with index %s looks like this:%s", str(self.analysis_id), PROCESS_MAP[str(self.analysis_id)])
+                self.assertTrue(filecmp.cmp(f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/logreader.log", f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/emba_logs/emba.log"), "Files not equal?!")   # check correctness of regexes
                 if re.match(STATUS_PATTERN, line):
                     self.assertEqual(logr.status_msg['status'], line)
                 elif re.match(PHASE_PATTERN, line):
                     self.assertEqual(logr.status_msg['phase'], line)
                 else:
                     print("weird line in logreader: %s", line)
-                _, phase_identifier = logr.phase_identify()
+                _, phase_identifier = logr.phase_identify(logr.status_msg)
                 if phase_identifier == EMBA_P_PHASE:
                     self.assertGreaterEqual(logr.status_msg['percentage'], 0.0)
                 elif phase_identifier == EMBA_S_PHASE:
@@ -52,10 +54,13 @@ class TestLogreader(TestCase):
                     self.assertGreaterEqual(logr.status_msg['percentage'], 0.50)
                 elif phase_identifier == EMBA_F_PHASE:
                     self.assertGreaterEqual(logr.status_msg['percentage'], 0.75)
-                elif phase_identifier == 4:
+                elif phase_identifier in (0,4):
                     self.assertEqual(logr.status_msg['percentage'], 1.0)
+                elif phase_identifier < 0:
+                    self.assertEqual(logr.status_msg['percentage'], 0.0)
                 else:
                     print("weird phase in logreader line: %s - phase: %s ", line, phase_identifier)
+                    raise Exception
             # logreader file should be identical to emba.log
             self.assertEqual(logr.finish, True)
 
@@ -66,3 +71,9 @@ class TestLogreader(TestCase):
         """
         with open(f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/emba_logs/emba.log", 'w', encoding='UTF-8') as file:
             file.write(line)
+
+    def test_logreader_with_files(self):
+        self.file_test(self.test_file_bad)
+        os.remove(f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/logreader.log")
+        os.remove(f"{settings.EMBA_LOG_ROOT}/{self.analysis_id}/emba_logs/emba.log")
+        self.file_test(self.test_file_good)
