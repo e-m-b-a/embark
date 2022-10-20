a5626ad292f2979b91277528c89b3b80c114bcfa
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-10-20 09:01:02 +0200

    add export skeleton

---
+++ embark/porter/exporter.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from pathlib import Path
+
+from django.conf import settings
+from django.core import serializers
+
+from dashboard.models import Result
+
+logger = logging.getLogger(__name__)
+
+def result_json(analysis_id):
+    """
+    returns file_name as str
+    """
+    if os.path.isdir(f"{settings.EMBA_LOG_ROOT}/{analysis_id}/"):
+        try:
+            with open(f"{settings.EMBA_LOG_ROOT}/{analysis_id}/export.json", 'w'):
+                data = serializers.serialize("json", Result.objects.filter(firmware_analysis=analysis_id))
+                #json write TODO
+                pass
+        except FileExistsError:
+            logger.error("File exists")
+    return Path(f"{analysis_id}/export.json")
