6ddb1ffc3f144dff069c686df0f39dd3e7b16c58
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-10-21 11:24:20 +0200

    rm fakepath

--- embark/static/scripts/fileUpload.js
+++ embark/static/scripts/fileUpload.js
@@ -32,7 +32,7 @@ $(window).bind("load", function() {
         document.querySelector("#file-input").onchange = function(){
             var fileNames = "";
             for (var i = 0; i < this.files.length; i++) {
-                fileNames = fileNames + this.value.name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + "<br>";
+                fileNames = fileNames + this.value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/^C:\\fakepath\\/i, '') + "<br>";
             }
             var target = document.querySelector("#file-name");
             target.innerHTML = fileNames;
