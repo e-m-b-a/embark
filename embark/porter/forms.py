7ae4e663868c6d3e4ca3bc1741ea0b024e8bf16c
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-11-03 08:09:29 +0100

    change widget

--- embark/porter/forms.py
+++ embark/porter/forms.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 class FirmwareAnalysisImportForm(forms.Form):
     zip_log_file = forms.ModelChoiceField(queryset=LogZipFile.objects.all(), empty_label='Select the zip-file for the import', required=True)
     firmware = forms.ModelChoiceField(queryset=FirmwareFile.objects.all(), empty_label='Select Firmware file the analysis is for', required=False)
-    device = forms.ModelMultipleChoiceField(queryset=Device.objects.all(), required=False)
+    device = forms.ModelMultipleChoiceField(queryset=Device.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
 
     version = forms.CharField(max_length=127, help_text="Firmware version", required=False)
-    notes = forms.CharField(max_length=127, help_text="Firmware version notes", required=False, widget=forms.CheckboxSelectMultiple)
+    notes = forms.CharField(max_length=127, help_text="Firmware version notes", required=False)
 
 
 class FirmwareAnalysisExportForm(forms.Form):
