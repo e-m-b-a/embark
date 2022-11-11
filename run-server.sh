8746b5f17de36a6fa99d62876aeae43cf2d86df8
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-11-11 06:54:20 +0100

    update alias proc

--- run-server.sh
+++ run-server.sh
@@ -244,11 +244,10 @@ pipenv run ./manage.py runmodwsgi --user www-embark --group sudo \
 --include-file /var/www/conf/embark.conf \
 --processes 4 --threads 4 \
 --graceful-timeout 5 \
---server-name embark.local \
+--server-name embark.local ${WSGI_FLAGS[@]} &
 # --ssl-certificate /var/www/conf/cert/embark.local --ssl-certificate-key-file /var/www/conf/cert/embark.local.key \
 # --https-port "$HTTPS_PORT" &
 #  --https-only --enable-debugger \
-${WSGI_FLAGS[@]} &
 sleep 5
 
 echo -e "\n[""$BLUE JOB""$NC""] Starting daphne(ASGI) - log to /embark/logs/daphne.log"
