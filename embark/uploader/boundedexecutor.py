6d54f1514154f890606791c352b76853cc195004
BenediktMKuehne <bennimail.4spam@gmail.com>
2022-11-25 15:08:38 +0100

    update boundedexec shutdown

--- embark/uploader/boundedexecutor.py
+++ embark/uploader/boundedexecutor.py
@@ -235,8 +235,15 @@ class BoundedExecutor:
     def shutdown(cls, wait=True):
         """See concurrent.futures.Executor#shutdown"""
         logger.info("shutting down Boundedexecutor")
-
         executor.shutdown(wait)
+        # set all running analysis to failed
+        running_analysis_list = FirmwareAnalysis.objects.filter(finished=False).exclude(failed=True)
+        for analysis_ in running_analysis_list:
+            analysis_.failed = True
+            analysis_.finished = True
+            analysis_.save()
+        logger.info("Shutdown successful")
+            
 
     @classmethod
     def csv_read(cls, analysis_id, path, cmd):
@@ -320,6 +327,6 @@ class BoundedExecutor:
 
     @classmethod
     @receiver(finish_execution, sender='system')
-    def sigint_handler(sender, **kwargs):
+    def sigint_handler(cls, sender, **kwargs):
         logger.info("Received shutdown signal in boundedexec")
-        pass
+        BoundedExecutor.shutdown()
