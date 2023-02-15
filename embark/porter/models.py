f02b939ce021aff589e7786bd7813d9690e6712c
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-02-15 18:00:51 +0100

    update log_zip path

--- embark/porter/models.py
+++ embark/porter/models.py
@@ -25,7 +25,7 @@ class LogZipFile(models.Model):
 
     def get_storage_path(self, filename):
         # file will be uploaded to MEDIA_ROOT/log_zip/<id>
-        return f"log_zip/{filename}"
+        return f"{settings.MEDIA_ROOT}/log_zip/{filename}"
 
     file = models.FileField(upload_to=get_storage_path)
 
