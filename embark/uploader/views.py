b3da633088731e9f6c5a7d10286b0cfd9ba41f0e
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-02-20 13:40:36 +0100

    add debug

--- embark/uploader/views.py
+++ embark/uploader/views.py
@@ -126,6 +126,8 @@ def start_analysis(request):
     """
     if request.method == 'POST':
         form = FirmwareAnalysisForm(request.POST)
+        logger.debug("Form: %s", form.is_valid())
+        logger.debug("Form: %s", form.errors)
         if form.is_valid():
             logger.debug("Posted Form is valid")
             logger.info("Starting analysis with %s", form.Meta.model.id)
