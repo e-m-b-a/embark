12eb01dd38ebb9aac73d0268ebf5158f83c9e875
Benedikt Kuehne <bennimail.4spam@gmail.com>
2023-05-04 17:10:44 +0200

    fix help thing

--- helper/helper_embark_general.sh
+++ helper/helper_embark_general.sh
@@ -110,7 +110,9 @@ check_db() {
     echo -e "---------------------------------------------------------------------------"
     echo -e "$CYAN""Old passwords are stored in the \"safe\" folder when uninstalling EMBArk""$NC\\n"
     echo -e "$CYAN""You could try recoverying manually by overwriting your\".env\" file""$NC\\n"
-    echo -e "$CYAN""The mysql-db was first started if the password(sha256sum): $(head -n1 ./embark_db/state.cnf) ""$NC\\n"
+    if [[ -f safe/history.env ]]; then
+      echo -e "$CYAN""The mysql-db was first started if the password(sha256sum): $(head -n1 ./safe/history.env | cut -d";" -f1) ""$NC\\n"
+    fi
     exit 1
   fi
 }
