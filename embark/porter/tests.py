21bf69597170c917057e0e39dde843941c332207
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-06-28 11:53:36 +0200

    update result check

--- embark/porter/tests.py
+++ embark/porter/tests.py
@@ -48,7 +48,7 @@ class TestImport(TestCase):
         self.assertTrue(result_obj)
         result_dict = dict(model_to_dict(result_obj))
         # check
-        self.assertDictContainsSubset(subset=result_dict["system_bin"], dictionary=self.test_result_dict)
+        self.assertTrue(result_dict["FW_path"] == self.test_result_dict["FW_path"])
 
     def test_zip_import(self):
         # first upload
