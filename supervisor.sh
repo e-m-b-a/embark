19af3b8d3883a02dd984a289192301b9b3f41a2f
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-11-30 13:46:50 +0000

    rm old dev interface

--- supervisor.sh
+++ supervisor.sh
@@ -20,7 +20,7 @@ exec 1<>${LOG_FILE}
 exec 2>&1
 
 while :; do
-  if ! ip a show embark_backend | grep -q "172.22.0.1" || ip a show embark_dev | grep -q "172.21.0.1"; then
+  if ! ip a show embark_backend | grep -q "172.22.0.1" ; then
     systemctl restart NetworkManager docker
     echo "$(date +"%D %T")""retstarted docker-networks"
   fi
