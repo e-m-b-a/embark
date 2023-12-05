b204c0aea9c74ab683de859c2683f301a501628a
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-12-05 14:20:49 +0000

    ignore shell for tz info

--- embark/embark/__init__.py
+++ embark/embark/__init__.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 def check_tz() -> bool:
     cmd = "date +%Z"
-    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE) as process:
+    with Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE) as process:   # nosec
         system_tz, _error = process.communicate()
         ret_code = process.returncode
     if ret_code != 0 :
