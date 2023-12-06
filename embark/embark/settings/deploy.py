eab9f4275894724b8c9cf5481e1cda0cf4f66062
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-12-06 12:13:43 +0000

    turn off debugging

--- embark/embark/settings/deploy.py
+++ embark/embark/settings/deploy.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
