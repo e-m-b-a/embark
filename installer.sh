4fd4c49a18e023621977f365d7f4333b830008d6
BenediktMKuehne <bennimail.4spam@gmail.com>
2023-12-13 15:01:23 +0000

    unused func

--- installer.sh
+++ installer.sh
@@ -68,7 +68,7 @@ import_helper(){
 }
 
 # Source: https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
-version(){ echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+# version(){ echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 save_old_env(){
   if ! [[ -d ./safe ]]; then
