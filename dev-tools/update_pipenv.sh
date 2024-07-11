92e7f81eb675d517e15278667ea8bf9cdec6e69d
BenediktMKuehne <bennimail.4spam@gmail.com>
2024-07-11 11:55:50 +0000

    add pipfile updater script

---
+++ dev-tools/update_pipenv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# EMBArk - The firmware security scanning environment
+#
+# Copyright 2024 Siemens Energy AG
+#
+# EMBArk comes with ABSOLUTELY NO WARRANTY.
+#
+# EMBArk is licensed under MIT
+#
+# Author(s): Benedikt Kuehne
+
+# Description: Automates updating the pipfile
+
+
+# 1. find out which installation
+local lINSTALL=""
+
+lINSTALL="deploy"
+
+if grep -q "EMBARK_INSTALL=dev" ./.env ; then
+  INSTALL="dev"
+fi
+
+# 2. update pipfile
+if [[ lINSTALL == "dev" ]]:
+  MYSQLCLIENT_LDFLAGS='-L/usr/mysql/lib -lmysqlclient -lssl -lcrypto -lresolv' MYSQLCLIENT_CFLAGS='-I/usr/include/mysql/' PIPENV_VENV_IN_PROJECT=1 pipenv update
+else
+  echo "This is not a developer setup...still trying to update"
+  pipenv update
+fi
+# push to web-dir if available
