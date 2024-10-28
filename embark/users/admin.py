2a470d9eae26c3554065d532af9f1d404a7fdbda
BenediktMKuehne <bennimail.4spam@gmail.com>
2024-10-28 17:49:13 +0000

    enable further admin options

--- embark/users/admin.py
+++ embark/users/admin.py
@@ -3,9 +3,10 @@ __author__ = 'Benedikt Kuehne'
 __license__ = 'MIT'
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import User, Team, TeamMember
 
-admin.site.register(User)
-admin.site.register(Team)
-admin.site.register(TeamMember)
+admin.site.register(User, UserAdmin)
+admin.site.register(Team, UserAdmin)
+admin.site.register(TeamMember, UserAdmin)
