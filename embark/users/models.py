15b8f5893ad89179e8f9ad84e02b382bad2be62a
BenediktMKuehne <bennimail.4spam@gmail.com>
2024-11-12 15:38:42 +0000

    add tracker-oermission

--- embark/users/models.py
+++ embark/users/models.py
@@ -33,6 +33,15 @@ class User(AbstractUser):
     is_active_member = models.BooleanField(default=True, help_text='Whether this team member is active or not')
 
     class Meta:
+        default_permissions = ()    # disable "add", "change", "delete" and "view" default permissions
         permissions = (
-            ("non_minimal", "Can access more functionalities of embark"),
+            ("user_permission", "Can access user menues of embark"),
+            ("tracker_permission", "Can access tracker functionalities of embark"),
+            ("updater_permission", "Can access updater functionalities of embark"),
+            ("uploader_permission_minimal", "Can access uploader functionalities of embark"),
+            ("uploader_permission_advanced", "Can access all uploader functionalities of embark"),
+            ("porter_permission", "Can access porter functionalities of embark"),
+            ("reporter_permission", "Can access reporter functionalities of embark"),
+            ("dashboard_permission_minimal", "Can access dashboard functionalities of embark"),
+            ("dashboard_permission_advanced", "Can access all dashboard functionalities of embark"),
         )
