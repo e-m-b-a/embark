95d194c8d2be479167b8a2316116058e0d997c75
BenediktMKuehne <bennimail.4spam@gmail.com>
2024-11-13 09:49:52 +0000

    rm pw vali for devs

--- embark/embark/settings/dev.py
+++ embark/embark/settings/dev.py
@@ -188,20 +188,11 @@ LOGGING = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
         "OPTIONS": {
-            "min_length": 8,
+            "min_length": 4,
         },
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    }
 ]
 
 # Internationalization
