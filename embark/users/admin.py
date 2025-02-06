64d7a3d58cd7a8c45ea744909b9aca870b722290
BenediktMKuehne <bennimail.4spam@gmail.com>
2025-02-06 11:59:27 +0000

    add missing fields to admin page

--- embark/users/admin.py
+++ embark/users/admin.py
@@ -7,5 +7,9 @@ from django.contrib.auth.admin import UserAdmin, GroupAdmin
 
 from users.models import User, Team
 
+UserAdmin.list_display += ('timezone', 'team_role',)
+UserAdmin.list_filter += ('timezone', 'team_role',)
+UserAdmin.fieldsets += ('timezone', 'team_role',)
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Team, GroupAdmin)
