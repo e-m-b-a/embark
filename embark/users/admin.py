5d2e95cc8a46edd6a8a6d77df7e147e2f12de647
BenediktMKuehne <bennimail.4spam@gmail.com>
2025-02-13 11:32:01 +0000

    form fix

--- embark/users/admin.py
+++ embark/users/admin.py
@@ -9,18 +9,21 @@ from django.contrib.sessions.models import Session
 
 from users.models import User, Team
 
+
 UserAdmin.list_display += ('timezone',)
 UserAdmin.list_filter += ('timezone',)
 UserAdmin.fieldsets += (("Custom", {"fields": ('timezone',)}),)
 
+
 class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
         return pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n')
-    _session_data.allow_tags=True
+    _session_data.allow_tags = True
     list_display = ['session_key', '_session_data', 'expire_date']
     readonly_fields = ['_session_data']
     exclude = ['session_data']
-    date_hierarchy='expire_date'
+    date_hierarchy = 'expire_date'
+
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Team)
