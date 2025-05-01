c569fbe5cf23ea06c202a5183061e293fd5d6e42
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2025-05-01 14:36:21 +0200

    Completing Issue#16 and waiting for review
    Signed-off-by: SirGankalot <73303677+SirGankalot@users.noreply.github.com>

---
+++ test/testing_sshfs/file_transfer.py
@@ -0,0 +1,23 @@
+# On the VM we need to use:
+
+# sudo apt update (optinoal)
+# sudo apt install openssh-server
+# sudo systemctl enable ssh
+# sudo systemctl start ssh
+# sudo apt install sshfs
+# pip install sshfs
+
+from sshfs import SSHFileSystem
+
+# Connect to the VM using SSHFS
+fs = SSHFileSystem(host='ip/localhost', username='name', password='password', port='the port that is opened')
+
+# Specify the remote file path and the local destination path
+remote_path = 'test'        # Path to the remote file on the VM
+local_path = r'test'      # Local path where the file will be saved
+
+with fs.open(remote_path, 'rb') as remote_file:
+    with open(local_path, 'wb') as local_file:
+        local_file.write(remote_file.read())
+
+print(f"File was successfully saved: {local_path}")
