8241e311deebe62966d84630ab95d06a38dc6543
ashiven <nevisha@pm.me>
2025-05-15 18:42:23 +0200

    implement connect_worker prototype
    Signed-off-by: ashiven <nevisha@pm.me>

--- embark/workers/models.py
+++ embark/workers/models.py
@@ -1,2 +1,33 @@
-# Create your models here.
+from django.db import models
+import uuid
+import ipaddress
+from django.core.exceptions import ValidationError
+
+from users.models import Configuration
+
+
 # TODO: add explicit makemigrations command for workers models in scripts i.e. debug-server-start.sh etc.
+
+class Worker(models.Model):
+    # each worker has a unique ID
+    worker_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # a worker can belong to exactly one configuration
+    # TODO: modify configuration models and views so a user can not 
+    # create multiple configs with the same ip range
+    configuration = models.ForeignKey(Configuration, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField()
+    system_info = models.JSONField()
+
+    def clean(self):
+        super().clean()
+        if self.configuration and self.configuration.ip_range:
+            try:
+                network = ipaddress.ip_network(self.configuration.ip_range, strict=False)
+                ip = ipaddress.ip_address(self.ip_address)
+                if ip not in network:
+                    raise ValidationError(
+                        {"ip_address": "IP address is not within the configuration's IP range."}
+                    )
+            except ValueError as e:
+                raise ValidationError({"configuration": f"Invalid IP range: {e}"})
--- embark/workers/views.py
+++ embark/workers/views.py
@@ -1,5 +1,64 @@
 from django.http import JsonResponse
 
+from workers.models import Worker
+
+# TODO: add dependency to SBOM and elsewhere
+import paramiko
+
+
+def register_new_worker(request):
+    """
+    Register a new worker. The configurations of the user are fetched and the 
+    worker automatically gets assigned the configuration whose IP range matches its
+    IP address. The worker is then added to the database and we return the ID assigned to the worker.
+    """
+    # TODO: implement
+    # we may alternatively implement it so that there is a worker scan for each
+    # configuration of the user and all detected workers are registered using 
+    # something like this endpoint
+
 
 def connect_worker(request, worker_id):
-    return JsonResponse({'worker_id': worker_id})
+    """
+    Connect to the worker with the given ID and gather system information.
+    This information is comprised of OS type and version, CPU count, RAM size, Disk size,
+    """
+    worker = Worker.objects.get(worker_id=worker_id)
+    worker_name = worker.name
+    worker_ip = worker.ip_address
+    configuration = worker.configuration
+    ssh_user = configuration.ssh_user
+    ssh_password = configuration.ssh_password
+
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(worker_ip, username=ssh_user, password=ssh_password)
+
+    _stdin, stdout, _stderr = ssh_client.exec_command("grep PRETTY_NAME /etc/os-release")
+    os_info = stdout.read().decode().strip()[len('PRETTY_NAME='):-1].strip('"')
+
+    _stdin, stdout, _stderr = ssh_client.exec_command("nproc")
+    cpu_info = stdout.read().decode().strip() + " cores"
+
+    _stdin, stdout, _stderr = ssh_client.exec_command("free -h | grep Mem")
+    ram_info = stdout.read().decode().strip()[len('Mem:'):].strip().split()[1]
+    ram_info = ram_info.replace('Gi', 'GB').replace('Mi', 'MB')
+
+    _stdin, stdout, _stderr = ssh_client.exec_command("df -h | grep '^/'")
+    disk_str = stdout.read().decode().strip().split('\n')[0].split()
+    disk_total = disk_str[1].replace('G', 'GB').replace('M', 'MB')
+    disk_free = disk_str[3].replace('G', 'GB').replace('M', 'MB')
+    disk_info = f"Total: {disk_total}, Free: {disk_free}"
+
+    ssh_client.close()
+
+    system_info = {
+        'os_info': os_info,
+        'cpu_info': cpu_info,
+        'ram_info': ram_info,
+        'disk_info': disk_info
+    }
+    worker.system_info = system_info
+    worker.save()
+
+    return JsonResponse({'worker_id': worker_id, 'worker_name': worker_name, 'system_info': worker.system_info})
