f6b90a69b51bc700e0a553b68dac904f6ea5e8f0
SirGankalot <73303677+SirGankalot@users.noreply.github.com>
2025-05-31 14:37:43 +0200

    Add fifo queue to the orchestrator and add a assign function where you dont choose the worker (the next free worker is choosen)
    Signed-off-by: SirGankalot <73303677+SirGankalot@users.noreply.github.com>

--- embark/workers/orchestrator.py
+++ embark/workers/orchestrator.py
@@ -1,21 +1,35 @@
-from typing import Dict
+from typing import Dict, Tuple
 from workers.models import Worker
+from collections import deque
 
 
 class WorkerOrchestrator:
     def __init__(self):
         self.dict_free_workers: Dict[str, Worker] = {}
-        self.dict_busy_workers: Dict[str, Worker] = {}
+        self.dict_busy_workers: Dict[str, Tuple[str, Worker]]
+        self.next_task = deque()
 
-    def get_busy_workers(self) -> Dict[str, Worker]:
+    def get_busy_workers(self) -> Dict[str, Tuple[str, Worker]]:
         return self.dict_busy_workers
 
     def get_free_workers(self) -> Dict[str, Worker]:
         return self.dict_free_workers
+    
+    def assign_task(self, task: str):
+        if not self.dict_free_workers:
+            if self.next_task:
+                self.next_task.append(task)
+                return ValueError("No free workers available, task added to the queue.")
+        
+        worker_ip, worker = next(iter(self.dict_free_workers.items()))
+        self.assign_worker(worker, task)
+        return worker_ip
 
-    def assign_worker(self, worker: Worker):
+    def assign_worker(self, worker: Worker, task: str):
+        if worker.ip_address not in self.dict_free_workers:
+            self.next_task.append(task)
         if worker.ip_address in self.dict_free_workers:
-            self.dict_busy_workers[worker.ip_address] = worker
+            self.dict_busy_workers[worker.ip_address] = (task, worker)
             del self.dict_free_workers[worker.ip_address]
         else:
             raise ValueError(f"Worker with IP {worker.ip_address} is already busy.")
