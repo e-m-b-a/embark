b65b536d19c4bc1bc01b6b16605b5ac35a92b673
ClProsser <clemens.prosser@gmail.com>
2025-05-31 20:57:33 +0200

    feat: Adjust worker file location
    Signed-off-by: ClProsser <clemens.prosser@gmail.com>

--- embark/embark/settings/deploy.py
+++ embark/embark/settings/deploy.py
@@ -311,6 +311,7 @@ SECURE_SSL_REDIRECT = False
 VERSION = get_version_strings()
 
 # Worker setup
-WORKER_SETUP_PATH = os.path.join(BASE_DIR.parent, "WORKER_SETUP")
-WORKER_SETUP_ZIP_PATH = os.path.join(BASE_DIR.parent, "WORKER_SETUP.tar.gz")
-WORKER_SETUP_LOGS = os.path.join(BASE_DIR, "worker_setup.log")
+WORKER_FILES_PATH = os.path.join(BASE_DIR.parent, "WORKER_FILES")
+WORKER_SETUP_PATH = os.path.join(WORKER_FILES_PATH, "WORKER_SETUP")
+WORKER_SETUP_ZIP_PATH = os.path.join(WORKER_FILES_PATH, "WORKER_SETUP.tar.gz")
+WORKER_SETUP_LOGS = os.path.join(WORKER_FILES_PATH, "worker_setup.log")
