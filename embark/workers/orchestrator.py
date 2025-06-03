43225ddbf0b6c4f397ece4cfefa3ee1e9a088bf7
ashiven <nevisha@pm.me>
2025-06-03 20:54:02 +0200

    update orchestrator and test files to current versions on main branch
    Signed-off-by: ashiven <nevisha@pm.me>

--- embark/workers/orchestrator.py
+++ embark/workers/orchestrator.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from collections import deque
 from workers.models import Worker
 
 
@@ -6,6 +7,7 @@ class WorkerOrchestrator:
     def __init__(self):
         self.dict_free_workers: Dict[str, Worker] = {}
         self.dict_busy_workers: Dict[str, Worker] = {}
+        self.queue_tasks = deque()
 
     def get_busy_workers(self) -> Dict[str, Worker]:
         return self.dict_busy_workers
@@ -25,6 +27,18 @@ class WorkerOrchestrator:
                 raise ValueError(f"Worker with IP {worker_ip} does not exist.")
         return map_worker_array
 
+    def assign_task(self, task: str):
+        if not self.dict_free_workers:
+            self.queue_tasks.append(task)
+        else:
+            free_worker = next(iter(self.dict_free_workers.values()))
+            if not self.queue_tasks:
+                self.assign_worker(free_worker, task)
+            else:
+                queued_task = self.queue_tasks.popleft()
+                self.assign_worker(free_worker, queued_task)
+                self.assign_task(task)
+
     def assign_worker(self, worker: Worker, task: str):
         if worker.ip_address in self.dict_free_workers:
             worker.job_id = task
@@ -36,8 +50,16 @@ class WorkerOrchestrator:
 
     def release_worker(self, worker: Worker):
         if worker.ip_address in self.dict_busy_workers:
-            self.dict_free_workers[worker.ip_address] = worker
-            del self.dict_busy_workers[worker.ip_address]
+            if self.queue_tasks:
+                next_task = self.queue_tasks.popleft()
+                self.dict_free_workers[worker.ip_address] = worker
+                del self.dict_busy_workers[worker.ip_address]
+                self.assign_worker(worker, next_task)
+            else:
+                self.dict_free_workers[worker.ip_address] = worker
+                del self.dict_busy_workers[worker.ip_address]
+                worker.job_id = None
+                worker.save()
         else:
             raise ValueError(f"Worker with IP {worker.ip_address} is not busy.")
 
