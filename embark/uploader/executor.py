2591fee6312dd64f740e7dc23d979c51f2fe738e
ClProsser <clemens.prosser@gmail.com>
2025-06-15 22:46:59 +0200

    feat: Add Orchestrator call to submit_firmware
    Signed-off-by: ClProsser <clemens.prosser@gmail.com>

--- embark/uploader/executor.py
+++ embark/uploader/executor.py
@@ -7,6 +7,7 @@ from uploader.models import FirmwareAnalysis, FirmwareFile
 from uploader.archiver import Archiver
 from uploader.boundedexecutor import BoundedExecutor
 from uploader.settings import workers_enabled
+from workers.orchestrator import WorkerOrchestrator, OrchestratorTask
 from embark.logreader import LogReader
 
 
@@ -41,13 +42,17 @@ def submit_firmware(firmware_analysis: FirmwareAnalysis, firmware_file: Firmware
     firmware_analysis.create_log_dir()
     firmware_analysis.set_meta_info()
 
-    emba_cmd = firmware_analysis.construct_emba_command(image_file_location)
-
     if workers_enabled():
-        # TODO: Trigger Orchestrator
+        target = f"{settings.WORKER_FIRMWARE_DIR}{emba_startfile.pop()}"
+        emba_cmd = firmware_analysis.construct_emba_command(target)
+
+        # TODO: Replace with actual Orchestrator instance
+        orchestrator = WorkerOrchestrator()
+        orchestrator.assign_task(OrchestratorTask(firmware_analysis, emba_cmd, image_file_location, target))
 
         return True
     else:
+        emba_cmd = firmware_analysis.construct_emba_command(image_file_location)
         emba_fut = BoundedExecutor.submit(BoundedExecutor.run_emba_cmd, emba_cmd, firmware_analysis.id, active_analyzer_dir)
         BoundedExecutor.submit(LogReader, firmware_analysis.id)
 
