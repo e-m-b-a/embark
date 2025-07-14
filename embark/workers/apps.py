581b9c2b840eb5e1ba82fbf06f15b559dbc3bfe3
ashiven <nevisha@pm.me>
2025-07-14 15:32:38 +0200

    reset dependency used_by and worker update queues om application start
    Signed-off-by: ashiven <nevisha@pm.me>

--- embark/workers/apps.py
+++ embark/workers/apps.py
@@ -19,3 +19,34 @@ class WorkersConfig(AppConfig):
             orchestrator = get_orchestrator()
             orchestrator.reset()
         post_migrate.connect(reset_orchestrator)
+
+        def reset_dependency_users(**kwargs):
+            """
+            Reset the 'used_by' field for all DependencyState instances.
+            An unexpected shutdown may leave this field populated with workers
+            that no longer use the dependency.
+            """
+            from workers.models import DependencyState  # pylint: disable=import-outside-toplevel
+
+            states = DependencyState.objects.all()
+            for state in states:
+                state.used_by.clear()
+                state.save()
+        post_migrate.connect(reset_dependency_users)
+
+        def reset_update_queues(**kwargs):
+            """
+            Reset the update queues for all workers.
+            An unexpected shutdown may leave the update queues
+            populated with updates that are no longer relevant.
+            """
+            from workers.models import Worker, WorkerUpdate  # pylint: disable=import-outside-toplevel
+
+            workers = Worker.objects.all()
+            for worker in workers:
+                update_queue = WorkerUpdate.objects.filter(worker__id=worker.id)
+                update_queue = [{
+                    "dependency_type": update.get_type().label,
+                    "version": update.version
+                } for update in update_queue]
+        post_migrate.connect(reset_update_queues)
