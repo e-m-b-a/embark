d5f0875b36dab3f54c779babadc395361375e477
BenediktMKuehne <bennimail.4spam@gmail.com>
2025-07-21 15:00:32 +0000

    implement simple password check skript for apache

--- embark/embark/wsgi_auth.py
+++ embark/embark/wsgi_auth.py
@@ -6,6 +6,26 @@ import django
 django.setup()
 
 from django.core.handlers.wsgi import WSGIHandler
-from django.contrib.auth.handlers.modwsgi import check_password  # pylint: disable=unused-import
 
 application = WSGIHandler()
+
+from users.models import User
+from django import db
+
+def check_password(environ, user, password):
+    db.reset_queries()
+
+    kwargs = {'username': user, 'is_active': True}
+
+    try:
+        try:
+            user = User.objects.get(**kwargs)
+        except User.DoesNotExist:
+            return None
+
+        if user.check_password(password):
+            return True
+        else:
+            return False
+    finally:
+        db.connection.close()
