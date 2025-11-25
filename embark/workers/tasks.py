f418763d3486fe1f52aa8594833ef96d3c8e075d
BenediktMKuehne <bennimail.4spam@gmail.com>
2025-11-25 14:40:38 +0000

    fix mkdir

--- embark/workers/tasks.py
+++ embark/workers/tasks.py
@@ -763,7 +763,7 @@ def _update_or_create_worker(config: Configuration, ip_address: str):
         )
         worker.save()
         # create log file
-        if not Path(settings.WORKER_LOG_ROOT_ABS).exists():
+        if not Path(os.path.join(settings.WORKER_LOG_ROOT_ABS, settings.WORKER_WORKER_LOGS)).exists():
             Path(os.path.join(settings.WORKER_LOG_ROOT_ABS, settings.WORKER_WORKER_LOGS)).mkdir(parents=True, exist_ok=True)
         worker.log_location = Path(f"{os.path.join(settings.WORKER_LOG_ROOT_ABS, settings.WORKER_WORKER_LOGS)}/{worker.id}.log")
         worker.write_log(f"\nCreated new worker for IP address {ip_address}\n")
